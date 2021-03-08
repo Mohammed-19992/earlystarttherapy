dba74f72db64a40abd95b164e620bbeb3b55cfae
Mohammed-19992 <abusalaharafat@hotmail.com>
2021-03-08 11:52:53 -0500

    adding new files

---
+++ early-start-therapy/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyAGTaZhrhQbqmqvDtgnUfEq2n4qdFcUiyg",
+    authDomain: "earlystarttherapy-21e46.firebaseapp.com",
+    databaseURL: "https://earlystarttherapy-21e46.firebaseapp.com",
+    projectId: "earlystarttherapy-21e46",
+    storageBucket: "earlystarttherapy-21e46.appspot.com",
+    messagingSenderId: "575626699351",
+    appId: "1:575626699351:web:775ef52055d1a7ccba7912",
+    measurementId: "G-B29RRLJCSW",
+  agora: {
+    appId: "1:575626699351:web:775ef52055d1a7ccba7912"
+  }
+  }
+
+};
